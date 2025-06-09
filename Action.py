62a5383f862920624e052f8290a15b0210dc77e8
FLTeather <frederica.teather@btinternet.com>
2025-06-09 13:48:39 +0100

    Added action class to handle information needed

---
+++ Action.py
@@ -0,0 +1,7 @@
+class Action:
+    def __init__(self, name, damage, range, atriType):
+        self.name = name # str
+        self.damage = damage # str
+        self.range = range # int
+        self.atriType = atriType #list of str
+
